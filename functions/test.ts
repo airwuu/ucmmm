91e78cb3ddabf58a2da5beb61788e071b8005482
Aaron <1kookyw1@gmail.com>
2024-10-10 20:10:43 -0700

    fix

--- functions/test.ts
+++ functions/test.ts
@@ -1,4 +1,4 @@
-export async function onRequestPost(context){
+export async function onRequestPost(context: { request: { formData: () => any } }){
     const formdata = await context.request.formData()
     const name = formdata.get('name')
     const email = formdata.get('email')
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <form action="/api/test" method="POST">
+      <form action="/test" method="POST">
         <input type="text" name="name" id=""></input>
         <input type="text" name="email" id=""></input>
         <input type="submit" value="submit"></input>

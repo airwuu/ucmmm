f5b94b4e6664d70faa5af2b5a5d653cccda06256
Aaron <1kookyw1@gmail.com>
2024-10-10 20:07:04 -0700

    cf pages preview

--- functions/test.ts
+++ functions/test.ts
@@ -1,4 +1,4 @@
-export async function onRequestPost(context: { request: { formData: () => any } }){
+export async function onRequestPost(context){
     const formdata = await context.request.formData()
     const name = formdata.get('name')
     const email = formdata.get('email')

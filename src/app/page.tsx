f9e1a9fd768a49f3e9c818d9dcbbc3db497f9bce
Aaron <1kookyw1@gmail.com>
2024-10-16 19:25:47 -0700

    displays current menu items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>{Date()}</p>
       <Demo/>
     </div>
   );

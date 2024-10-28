fe05addef5583a12db8b99e66d6dbc047828966c
Aaron <1kookyw1@gmail.com>
2024-10-27 18:51:14 -0700

    footer change

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,13 +49,12 @@ export default function RootLayout({
             </main>
             <footer className="w-full flex items-center justify-center py-3">
               <Link
-                isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                href=""
                 title="nextui.org homepage"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
+                <span className="text-default-600">Made with love by</span>
+                <p className="text-primary">ucmmm</p>
               </Link>
             </footer>
           </div>

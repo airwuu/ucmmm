02bae4e25cab8b7634dfbb06ee43bb4b57032551
Aaron <1kookyw1@gmail.com>
2024-10-28 01:31:58 -0700

    moved menu api requests to functions

--- components/container.tsx
+++ components/container.tsx
@@ -5,14 +5,16 @@ import {ScrollShadow} from "@nextui-org/react";
 
 interface containerProps{
     pavMenuItems: any;
+    pavTime: any;
     dcMenuItems: any;
+    dcTime: any;
 }
-const container: React.FC<containerProps> = ({pavMenuItems,dcMenuItems}) => {
+const container: React.FC<containerProps> = ({pavMenuItems,pavTime="",dcMenuItems,dcTime=""}) => {
   return (
       <ScrollShadow className="h-full scrollbar-hide flex snap-mandatory snap-x overflow-x-auto w-full px-36 gap-3 py-2">
-        <MainCard location="Pavilion" items={pavMenuItems}/>
-        <MainCard location="YWDC" items={dcMenuItems}/>
-        <MainCard location="Food Trucks (WIP)" items={dcMenuItems}/>
+        <MainCard location="Pavilion" items={pavMenuItems} text={pavTime}/>
+        <MainCard location="YWDC" items={dcMenuItems} text={dcTime}/>
+        <MainCard location="Food Trucks (WIP)" items={dcMenuItems} text=""/>
       </ScrollShadow>
   )
 }

d4373233ec45f96f75031a4813f09520ca96b0c4
Aaron <1kookyw1@gmail.com>
2024-11-05 20:57:55 -0800

    report button works

--- components/item.tsx
+++ components/item.tsx
@@ -7,6 +7,15 @@ interface itemProps{
     id : any;
 }
 const item: React.FC<itemProps> = ({name, reports, id}) => {
+
+  function report(){
+    fetch(`https://ucmmmdb.ucmmm-ucm.workers.dev/item/${id}/missing`, {
+        method: "POST",
+      })
+        .then(response => response.json()) // if expecting a JSON response
+        .then(data => console.log(data))
+        .catch(error => console.error("Error:", error));
+  }
   return (
     <Dropdown backdrop="blur">
       <DropdownTrigger>
@@ -23,7 +32,7 @@ const item: React.FC<itemProps> = ({name, reports, id}) => {
         <DropdownItem key="name"><b>{name}</b></DropdownItem>
         <DropdownItem key="id">id: {id}</DropdownItem>
         <DropdownItem key="reports">Reports: {reports}</DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
+        <DropdownItem key="delete" className="text-danger" color="danger" onClick={report}>
           Report Missing
         </DropdownItem>
       </DropdownMenu>

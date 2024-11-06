01c92938b5f58409eeebaee04d8b0e3abd521901
airwuu <1kookyw1@gmail.com>
2024-11-05 23:10:47 -0800

    potential fix?

--- config/site.ts
+++ config/site.ts
@@ -21,5 +21,9 @@ export const siteConfig = {
   ],
   links: {
     github: "https://github.com/airwuu/ucmmm",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
   },
 };

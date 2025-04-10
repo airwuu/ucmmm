288998c63efc64f3254f03ebca3251a5bb6bbb06
Aaron <1kookyw1@gmail.com>
2025-04-10 15:47:19 -0700

    autoscroll to dc

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Cards from "@/components/cards";
+import Snap from "@/components/snap"
 import { Suspense } from "react";
 
 export default function Home() {
@@ -6,10 +6,7 @@ export default function Home() {
     <section className="h-full flex flex-col items-center justify-center gap-4 py-2 md:py-4">
       <div className="h-full inline-block max-w-xl md:max-w-full text-center justify-center">
         <Suspense fallback={<div>Loading...</div>}>
-          <div className="flex snap-mandatory snap-x overflow-x-auto w-full px-36 gap-3 py-2 overflow-y-auto scrollbar-hide">
-            <Cards name="Pavilion" location="pav" />
-            <Cards name="Dining Center" location="dc" />
-          </div>
+          <Snap/>
         </Suspense>
       </div>
     </section>
